e3186bb35469f1b38821365a85a0815622bc08e7
dtk <dtk@gmx.de>
2014-05-04 16:53:50 +0200

    Introduce `tox`
    Create a basic `setup.py` to satisfy tox.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+    name = "ctx_parser",
+    version = "0.1",
+    packages = find_packages(),
+
+    # metadata for upload to PyPI
+    author = "dtk",
+    description = "A parser for `.ctx` files",
+    keywords = "ctx aqbanking",
+    url = "https://github.com/0x64746b/ctx_parser",
+)
