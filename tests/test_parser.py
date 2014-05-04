bbe3fc19515ed4c345d0b9df8ef6ed9d80b39d1b
dtk <dtk@gmx.de>
2014-05-04 19:14:55 +0200

    Extend `LIST` parsing test
    Add another element to list. Check type of parsed value.

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -5,6 +5,7 @@ from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
+from six import string_types
 import unittest
 
 from ctx_parser import parser
@@ -14,20 +15,42 @@ class TestTheParser(unittest.TestCase):
 
     def test_can_parse_lists(self):
         # setup test data
-        test_string = 'test_value'
+        test_string_1 = 'test_string_1'
+        test_string_2 = 'test_string_2'
+
         test_list = """
           testElementList {
             testElement {
-                char string="%(string_value)s"
+                char oneValue="%(first_value)s"
+            } #testElement
+
+            testElement {
+                char anotherValue="%(second_value)s"
             } #testElement
           } #testElementList
-          """ % {'string_value': test_string}
+        """ % {
+            'first_value': test_string_1,
+            'second_value': test_string_2,
+        }
 
-        # test parser
+        # test parsing of list
         parsed_elements = parser.LIST.parseString(test_list)
 
         # check result
         self.assertEqual(
-            parsed_elements.testElementList[0].string,
-            test_string
+            parsed_elements.testElementList[0].oneValue,
+            test_string_1
+        )
+        self.assertIsInstance(
+            parsed_elements.testElementList[0].oneValue,
+            string_types
+        )
+
+        self.assertEqual(
+            parsed_elements.testElementList[1].anotherValue,
+            test_string_2
+        )
+        self.assertIsInstance(
+            parsed_elements.testElementList[1].anotherValue,
+            string_types
         )
